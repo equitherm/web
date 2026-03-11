a8ad689f33d4db1e6949ae5674f6d3bb37b4941a
Thomas SAMTER <7680607+P4uLT@users.noreply.github.com>
2026-03-11 20:27:31 +0100

    Add GitHub Pages deployment (#8)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/equitherm-studio/',
   plugins: [react()],
   resolve: {
     alias: {
